95ece2582840afc4c553576ee9ad375fd8a458eb
Eduard <claygod@yandex.ru>
2016-12-08 15:24:34 +0300

    Add files via upload

--- tools/exchange_structs.go
+++ tools/exchange_structs.go
@@ -4,29 +4,35 @@ package tools
 // Reply structures
 // Copyright © 2016 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 
+// Article - send in aggregate
 type Article struct {
 	Id    string
 	Title string
 	Text  interface{}
 }
 
+// List - key-article send in menu
 type List map[string]string
 
+// NewPart - create HandlerPart
 func NewPart(name TypeSERVICE) *HandlerPart {
 	return &HandlerPart{PartName: name}
 }
 
+// HandlerPart to simplify the configuration of the handler
 type HandlerPart struct {
 	PartName     TypeSERVICE
 	PartTheme    TypeTHEME
 	PartTemplate string
 }
 
+// Theme - set theme
 func (hp *HandlerPart) Theme(theme TypeTHEME) *HandlerPart {
 	hp.PartTheme = theme
 	return hp
 }
 
+// Template - set template
 func (hp *HandlerPart) Template(path string) *HandlerPart {
 	hp.PartTemplate = path
 	return hp
