5a10cd20262f785c89b9ba836a819c04b1c3a63c
Eduard <claygod@yandex.ru>
2016-12-08 15:17:05 +0300

    Add files via upload

--- tools/message.go
+++ tools/message.go
@@ -6,8 +6,6 @@ package tools
 
 import "time"
 
-//import "github.com/claygod/mmoa/tools"
-
 // Message - main struct.
 type Message struct {
 	MsgCid        TypeCID // correlation identifier
@@ -26,32 +24,43 @@ func NewMessage() *Message {
 	return m
 }
 
+// Cid - correlation identifier
 func (m *Message) Cid(cid TypeCID) *Message {
 	m.MsgCid = cid
 	return m
 }
 
+// From - sende
 func (m *Message) From(from TypeSERVICE) *Message {
 	m.AddsFrom = from
 	return m
 }
 
+// To - destination
 func (m *Message) To(to TypeSERVICE) *Message {
 	m.AddsTo = to
 	return m
 }
+
+// Re - where to send the response
 func (m *Message) Re(re TypeSERVICE) *Message {
 	m.AddsRe = re
 	return m
 }
+
+// Theme - the meggage theme
 func (m *Message) Theme(theme TypeTHEME) *Message {
 	m.MsgTheme = theme
 	return m
 }
+
+// Field - the contents of the letter
 func (m *Message) Field(key string, value interface{}) *Message {
 	m.MsgCtx[key] = value
 	return m
 }
+
+// StatusCode - response status
 func (m *Message) StatusCode(h int) *Message {
 	m.MsgStatusCode = h
 	return m
