a16e469409d292106d37327cecb4751089a2d5f9
Eduard <claygod@yandex.ru>
2016-12-08 15:39:00 +0300

    Add files via upload

--- support/aggregator.go
+++ support/aggregator.go
@@ -4,14 +4,9 @@ package support
 // Aggregator
 // Copyright © 2016 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 
-//import "reflect"
-//import "sync"
-//import "time"
 import "errors"
 import "github.com/claygod/mmoa/tools"
 import "github.com/claygod/mmoa/service"
-
-//import "fmt"
 
 // NewAggregator - create a new Aggregator
 func NewAggregator(chIn chan *tools.Message, chBus chan *tools.Message) *Aggregator {
@@ -35,38 +30,12 @@ type Aggregator struct {
 
 // Aggregate - add a new aggregate in The aggregator
 func (a *Aggregator) Aggregate(cid tools.TypeCID, duration tools.TypeTIME, messages map[string]*tools.Message, ch chan *tools.Message) error {
-	//currentTime := tools.TypeTIME(time.Now().UnixNano())
-	//fmt.Print("\n", a.The.Service.Controller, a.The.Service)
 	msg := tools.NewMessage().Cid(cid).
 		From(a.Name).To(a.The.Service.Controller).
 		Theme(a.The.Aggregator.Filled)
-	//ae := service.NewAggregate(duration, messages, ch)
-	/*
-		ae := &Aggregate{
-			0,
-			//cid,
-			currentTime,
-			currentTime + duration,
-			messages,
-			len(messages),
-			ch,
-		}
-	*/
-	//a.WaitingFor.Lock()
-	//if _, ok := a.WaitingFor.Arr[cid]; ok {
-	//	a.WaitingFor.Unlock()
-	//	return errors.New("Aggregate with the cID already exists")
-	//}
-	//wg := &service.Waiting{
-	//	ae,
-	//	msg,
-	//}
 
 	if err := a.WaitingFor.NewWaiting(msg, duration, messages, ch); err != nil {
 		return errors.New("Aggregate with the cID already exists")
 	}
-
-	//a.WaitingFor.Arr[msg.MsgCid] = wg
-	//a.WaitingFor.Unlock()
 	return nil
 }
